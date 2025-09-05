541314b0c9863a295241df41a4df6e47f945749b
Cameron Smith <cameron.smith3030@gmail.com>
2025-09-04 22:01:22 -0400

    add app css file, started app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
 export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addCart(plant) {
+    const plantExists = cart.find((i) => i.id === plant.id);
+
+    if (plantExists) {
+      setCart();
+    } else {
+      const item = { ...plant, quantity: 1 };
+      setCart([...cart, item]);
+    }
+  }
+
   return <></>;
 }
